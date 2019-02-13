92d0575abfe13c189859b5f162a932c1ab7eaea1
Wprofessor <wprofessor@gmail.com>
2019-02-13 19:46:01 +0800

    Wprofessor commit.

--- app/src/main/java/com/example/ewew/wechat/WeChat.java
+++ app/src/main/java/com/example/ewew/wechat/WeChat.java
@@ -3,13 +3,19 @@ package com.example.ewew.wechat;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class WeChat extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private FragmentManager fragmentManager;
+    private Fragment chatsFragment;
+    private Fragment contactsFragment;
+    private Fragment discoverFragment;
+    private Fragment meFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,16 +24,16 @@ public class WeChat extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.chats_name:
-                    mTextMessage.setText(R.string.title_chats);
+                    fragmentManager.beginTransaction().replace(R.id.fragment_container,chatsFragment).commit();
                     return true;
                 case R.id.Contacts_name:
-                    mTextMessage.setText(R.string.title_Contacts);
+                    fragmentManager.beginTransaction().replace(R.id.fragment_container,contactsFragment).commit();
                     return true;
                 case R.id.Discover_name:
-                    mTextMessage.setText(R.string.title_Discover);
+                    fragmentManager.beginTransaction().replace(R.id.fragment_container,discoverFragment).commit();
                     return true;
                 case R.id.me_name:
-                    mTextMessage.setText(R.string.title_Me);
+                    fragmentManager.beginTransaction().replace(R.id.fragment_container,meFragment).commit();
                     return true;
             }
             return false;
@@ -39,9 +45,23 @@ public class WeChat extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_we_chat);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        /**
+         * 初始化
+         */
+        fragmentManager = getSupportFragmentManager();
+        // 多态之向上转型
+        chatsFragment = new Chatsfragment();
+        contactsFragment = new Contactsfragment();
+        discoverFragment = new Discoverfragment();
+        meFragment = new Mefragment();
+
+        //将碎片添加到fragmentmanger中
+        fragmentManager.beginTransaction().add(R.id.fragment_container,chatsFragment).commit();
+        fragmentManager.beginTransaction().add(R.id.fragment_container,contactsFragment).commit();
+        fragmentManager.beginTransaction().add(R.id.fragment_container,discoverFragment).commit();
+        fragmentManager.beginTransaction().add(R.id.fragment_container,meFragment).commit();
     }
 
 }
